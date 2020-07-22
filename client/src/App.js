ec98cf657cb5d5ae97dfda49a53669ded0632085
Andrew Blocki <arblocki@umich.edu>
2020-07-22 18:11:36 -0400

    Added GA view tracking

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar.js'; 
 import PageHeader from './components/PageHeader/PageHeader.js';
 import ScoresContainer from './containers/ScoresContainer.js';
+import ReactGA from 'react-ga';
 
 import './assets/css/styles.css';
- 
 import "react-datepicker/dist/react-datepicker.css";
+
+ReactGA.initialize('UA-173205165-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 class App extends Component {
 
